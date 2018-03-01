eca5ba29a6625749d5d922b7b1759a05cf65fb6a
Козырев Михаил Михайлович <ip714s13@mwp01.csc.local>
2018-03-01 14:13:39 +0700

    Make programm for chess

--- main.c
+++ main.c
@@ -3,12 +3,12 @@
 
 int main()
 {
-  printf("0./n/n");
-  printf("8 r n b q k b n r /n");
-  printf("7 p p p p p p p p /n");
-  printf("6/n5/n4/n3/n");
-  printf("2 P P P P P P P P/n");
-  printf("1 R N B Q K B N R/n");
-  printf("a b c d e f g h/n");
+  printf("0.\n\n");
+  printf("8 r n b q k b n r \n");
+  printf("7 p p p p p p p p \n");
+  printf("6\n5\n4\n3\n");
+  printf("2 P P P P P P P P\n");
+  printf("1 R N B Q K B N R\n");
+  printf("a b c d e f g h\n");
   return 0;
 }
