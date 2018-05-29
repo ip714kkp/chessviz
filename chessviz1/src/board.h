0357cc365de5349468d107260f2878785fe42ee2
Козырев Михаил Михайлович <ip714s13@mwp10.csc.local>
2018-05-29 15:00:37 +0700

    Fix makefile and board.h

--- chessviz1/src/board.h
+++ chessviz1/src/board.h
@@ -4,8 +4,8 @@
 #include <string.h>
 #define black_color printf("%s","\033[1;30m")
 #define white_color printf("%s","\033[1;37m")
-#define blue_color printf("%s","\033[1;36m")
-#define zero_color printf("%s","\033[0m")
+#define blue_color  printf("%s","\033[1;36m")
+#define zero_color  printf("%s","\033[0m")
 int move_check(char figure, int int_c, int r, int int_c_end, int r_end);
 void printBoard(char* board[][8]);
 void vvod(char* board[][8]);
